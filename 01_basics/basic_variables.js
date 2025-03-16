1fc2f0692359676d22104f52981c94e2622b2bac
the-rohit-kumar <rohitkumar210103@gmail.com>
2025-03-16 15:52:25 +0530

    Day-2

---
+++ 01_basics/basic_variables.js
@@ -0,0 +1,12 @@
+const set=69
+let email="abc@gmail.com"
+let number=22
+
+let state= true
+
+// set=89
+console.log(set);
+state=false
+
+
+console.log([set,email,number,state]);
---
+++ 01_basics/conversionTypes.js
@@ -0,0 +1,11 @@
+const set = 69;
+let email = "abc@gmail.com";
+let number = "22";
+let number2 = Number(number);
+
+let state = true;
+
+console.log(typeof set);
+console.log(typeof email);
+console.log(typeof number);
+console.log(typeof number2);
