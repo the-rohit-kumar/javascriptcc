bc0565e61a20ee97655b993ea3f72ee027c15877
Rohit Kumar <112833767+the-rohit-kumar@users.noreply.github.com>
2025-03-18 09:02:37 +0000

    day-3

--- 01_basics/conversionTypes.js
+++ 01_basics/conversionTypes.js
@@ -5,7 +5,25 @@ let number2 = Number(number);
 
 let state = true;
 
-console.log(typeof set);
-console.log(typeof email);
-console.log(typeof number);
-console.log(typeof number2);
+// console.log(typeof set);
+// console.log(typeof email);
+// console.log(typeof number);
+// console.log(typeof number2);
+
+// 1->true  0->false
+// ""->false "Something..."->true
+
+console.log(("1" + 2) + 2);
+console.log(+false);
+
+
+
+let x = 3n;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+let x2 = 3n;
+const y2 = x2++;
+console.log(`x2:${x2}  y2: ${y2}`);
+
---
+++ 01_basics/strings.js
@@ -0,0 +1,15 @@
+const myname = "Rohit";
+const repoCount = 50;
+
+const gamename=new String(`rohit`)
+
+console.log(myname, repoCount);
+
+console.log(`Name: ${myname.length},RepoCount: ${repoCount}`);
+
+
+const newString=gamename.slice(-3,5)
+const newString1=gamename.substring(0,5)
+console.log(gamename.charAt(2));
+console.log(newString);
+console.log(newString1);
